ef096267b1a2996b4c1e8b71b4b5fe25c030b99b
weixinmo <33052962+weixinmo@users.noreply.github.com>
2019-09-02 02:49:07 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function addItem() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    $('.shopping-list').append(
+      '<li>' + 
+      '<span class="shopping-item">' + $('#shopping-list-entry').val() + '</span>' +
+      '<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' + '<span class="button-label">' + 'check' +
+      '</span>' + '</button>' + '<button class="shopping-item-delete">' +
+      '<span class="button-label">' + 'delete' + '</span>' + '</button>' +
+      '</div>' + '</li>'
+    );
+  })
+}
+
+function removeItem() {
+  $('ul').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  })
+}
+
+function checkItem() {
+  $('ul').on('click', '.shopping-item-toggle', function() {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  })
+}
+
+$(addItem);
+$(removeItem);
+$(checkItem);
